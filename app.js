e399bffd6e8ff3ab7c0b0c535a864ecc5c8fa25f
William Chong <williamchong007@msn.com>
2015-01-30 15:07:50 +0800

    handle bar

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname+'/public'));
 app.get('/', function (req,res) {
-	res.sendFile(__dirname+'/public/main.html');
+	res.render('home');
+})
+app.get('/main', function (req,res) {
+	res.render('home');
+})
+app.get('/product', function (req,res) {
+	res.render('product');
 })
 
+
 var server = app.listen(process.env.PORT||80, function() {
-	var host = server.address().address
-	var port = server.address().port
+	var host = server.address().address;
+	var port = server.address().port;
 	console.log('Running at %s:%s', host ,port);
 })
 
