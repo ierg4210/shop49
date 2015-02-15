1ab12dfa1b267535d91d41733fca7af963e9117c
William Chong <williamchong007@msn.com>
2015-02-16 01:44:33 +0800

    validate

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var config = require('../shop49.config.js');
 var anyDB = require('any-db');
+var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var pool = anyDB.createPool("mysql://shop49-admin:01020304@aaa1nzh6y3zqur.cftehalxuulr.ap-southeast-1.rds.amazonaws.com/shop49",{
 	min:2, max:20
@@ -35,7 +36,7 @@ var inputPattern = {
 };
 
 // for parsing application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.urlencoded({extended:true}));
 // this line must be immediately after express.bodyParser()!
 // Reference: https://www.npmjs.com/package/express-validator
 router.use(expressValidator());
