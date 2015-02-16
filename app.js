94004fc793321271952aad680422a1c52fdcda24
William Chong <williamchong007@msn.com>
2015-02-16 10:57:42 +0800

    api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var exphbs  = require('express-handlebars');
 var mainRouter = require('./routes/main.js')
 var adminRouter = require('./routes/admin.js')
+var apiRouter = require('./routes/api.js')
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -10,6 +11,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname+'/public'));
 app.use('/',mainRouter);
 app.use('/admin',adminRouter);
+app.use('/admin/api',apiRouter);
 
 var server = app.listen(process.env.PORT||80, function() {
 	var host = server.address().address;
